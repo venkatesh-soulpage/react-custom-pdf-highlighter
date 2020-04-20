79fedc0fd16c8304f9781f55b42c5c33939b11aa
mohan <mohan@xorstack.com>
2020-04-20 19:33:56 +0530

    commit

--- src/components/PdfHighlighter.js
+++ src/components/PdfHighlighter.js
@@ -514,7 +514,7 @@ var PdfHighlighter = function (_PureComponent) {
         }) : null
       ),
       _react2.default.createElement("div", { className: "outer-container" },
-      _ToolbarController2,
+      _react2.default.createElement(_ToolbarController2.default,{viewer:this.viewer}),
       ),
     );
   };
