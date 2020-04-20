c9dd62e4a34cf4a04928072fa81ad99be505839d
mohan <mohan@xorstack.com>
2020-04-20 17:52:16 +0530

    commit

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,7 +1,59 @@
 import React from 'react'
 
 function ToolBar() {
-  return <div className="texct-dark">Our package</div>
+  return (
+    <div className="container rounded text-white" style={{backgroundColor: "#343a40bf"}}>
+    <div className="row">
+      <div className="col-sm-4 text-center">
+        <div className="btn-group" role="group">
+          <button type="button" className="btn btn-sm btn-link text-white pr-2" onClick={()=>zoomMinus()}>
+            <i className="material-icons">zoom_out</i>
+          </button>
+          <button type="button" className="btn btn-sm btn-link text-white px-2" onClick={()=>zoomReset()}>
+            <i className="material-icons">refresh</i>
+          </button>
+          <button type="button" className="btn btn-sm btn-link text-white pl-2" onClick={()=>zoomPlus()}>
+            <i className="material-icons">zoom_in</i>
+          </button>
+        </div>
+      </div>
+      <div className="col-sm-4 text-center">
+        <div className="btn-group" role="group">
+          <button className="btn btn-sm btn-link text-white pr-2" onClick={()=>previousPage()}>
+            <i className="material-icons">keyboard_arrow_left</i>
+          </button>
+          <div class="small py-3">Page  {currentPageNumber} / {pagesCount}</div>
+          <button className="btn btn-sm btn-link text-white pl-2" onClick={()=>nextPage()}>
+            <i className="material-icons">keyboard_arrow_right</i>
+          </button>
+        </div>
+      </div>
+      <div className="col-sm-4 text-center">
+        <div className="btn-group" role="group">
+          <button type="button" className="btn btn-sm btn-link text-white pr-2" onClick={()=>rotateLeft()}>
+            <i className="material-icons">rotate_left</i>
+          </button>
+          <button type="button" className="btn btn-sm btn-link text-white px-2" onClick={()=>rotateReset()}>
+            <i className="material-icons">refresh</i>
+          </button>
+          <button type="button" className="btn btn-sm btn-link text-white pl-2" onClick={()=>rotateRight()}>
+            <i className="material-icons">rotate_right</i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default ToolBar
+
+function zoomMinus(){return false}
+function zoomReset(){return false}
+function zoomPlus(){return false}
+function previousPage(){return false}
+function nextPage(){return false}
+function rotateLeft(){return false}
+function rotateReset(){return false}
+function rotateRight(){return false}
+
