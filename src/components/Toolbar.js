047ad2bd208896ed3c45ac0896ac8c4061adcb03
mohan <mohan@xorstack.com>
2020-04-20 16:46:02 +0530

    commit

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,28 +1,49 @@
-"use strict";
+import React, { Component } from 'react'
 
-exports.__esModule = true;
-
-var _react = require("react");
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var Toolbar = function (_Component) {
-
-    Toolbar.prototype.render = function render() {
-
-        return _react2.default.createElement(
-            "div",
-            {
-                className: "container rounded text-white",
-                style:{backgroundColor: "#343a40bf"}
-            },
-        );
-    };
-
-    return Toolbar;
-}(_react.Component);
-
-exports.default = Toolbar;
-module.exports = exports["default"];
+export default class Toolbar extends Component {
+    render() {
+        return (
+            <div className="container rounded text-white" style={{backgroundColor: "#343a40bf"}}>
+              <div className="row">
+                <div className="col-sm-4 text-center">
+                  <div className="btn-group" role="group">
+                    <button type="button" className="btn btn-sm btn-link text-white pr-2" >
+                      <i className="material-icons">zoom_out</i>
+                    </button>
+                    <button type="button" className="btn btn-sm btn-link text-white px-2" >
+                      <i className="material-icons">refresh</i>
+                    </button>
+                    <button type="button" className="btn btn-sm btn-link text-white pl-2" >
+                      <i className="material-icons">zoom_in</i>
+                    </button>
+                  </div>
+                </div>
+                <div className="col-sm-4 text-center">
+                  <div className="btn-group" role="group">
+                    <button className="btn btn-sm btn-link text-white pr-2" >
+                      <i className="material-icons">keyboard_arrow_left</i>
+                    </button>
+                    <div class="small py-3">Page  {`@`} / {`@`}</div>
+                    <button className="btn btn-sm btn-link text-white pl-2" >
+                      <i className="material-icons">keyboard_arrow_right</i>
+                    </button>
+                  </div>
+                </div>
+                <div className="col-sm-4 text-center">
+                  <div className="btn-group" role="group">
+                    <button type="button" className="btn btn-sm btn-link text-white pr-2" >
+                      <i className="material-icons">rotate_left</i>
+                    </button>
+                    <button type="button" className="btn btn-sm btn-link text-white px-2" >
+                      <i className="material-icons">refresh</i>
+                    </button>
+                    <button type="button" className="btn btn-sm btn-link text-white pl-2" >
+                      <i className="material-icons">rotate_right</i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+        )
+    }
+}
