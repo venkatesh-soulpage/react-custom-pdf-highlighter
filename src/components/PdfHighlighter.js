b63e118e312aad9692f2440d7c2b533ba14fe616
mohan <mohan@xorstack.com>
2020-04-20 16:14:22 +0530

    update

--- src/components/PdfHighlighter.js
+++ src/components/PdfHighlighter.js
@@ -28,6 +28,8 @@ require("../style/pdf_viewer.css");
 
 require("../style/PdfHighlighter.css");
 
+require("../style/App.css");
+
 var _getBoundingRect = require("../lib/get-bounding-rect");
 
 var _getBoundingRect2 = _interopRequireDefault(_getBoundingRect);
@@ -505,7 +507,49 @@ var PdfHighlighter = function (_PureComponent) {
           }
         }) : null
       ),
-      _react2.default.createElement("div", { className: "outer-container" }),
+      _react2.default.createElement("div", { className: "outer-container" },
+      _react2.default.inner(`  <div className="container rounded text-white" style={{backgroundColor: "#343a40bf"}}>
+      <div className="row">
+        <div className="col-sm-4 text-center">
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-sm btn-link text-white pr-2" >
+              <i className="material-icons">zoom_out</i>
+            </button>
+            <button type="button" className="btn btn-sm btn-link text-white px-2" >
+              <i className="material-icons">refresh</i>
+            </button>
+            <button type="button" className="btn btn-sm btn-link text-white pl-2" >
+              <i className="material-icons">zoom_in</i>
+            </button>
+          </div>
+        </div>
+        <div className="col-sm-4 text-center">
+          <div className="btn-group" role="group">
+            <button className="btn btn-sm btn-link text-white pr-2" >
+              <i className="material-icons">keyboard_arrow_left</i>
+            </button>
+            <div class="small py-3">Page  {currentPageNumber} / {pagesCount}</div>
+            <button className="btn btn-sm btn-link text-white pl-2" >
+              <i className="material-icons">keyboard_arrow_right</i>
+            </button>
+          </div>
+        </div>
+        <div className="col-sm-4 text-center">
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-sm btn-link text-white pr-2" >
+              <i className="material-icons">rotate_left</i>
+            </button>
+            <button type="button" className="btn btn-sm btn-link text-white px-2" >
+              <i className="material-icons">refresh</i>
+            </button>
+            <button type="button" className="btn btn-sm btn-link text-white pl-2" >
+              <i className="material-icons">rotate_right</i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>`)
+      ),
     );
   };
 
