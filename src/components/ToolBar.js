a7f885c8a7bc1f16a0aeb1c436ddfa012b50c7ef
mohan <mohan@xorstack.com>
2020-04-20 17:45:19 +0530

    commit

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,9 +1,7 @@
-var createReactClass = require('create-react-class');
-var ToolBar = createReactClass({
-  render: function() {
-    return (
-        <div>
-        </div>
-    );
-  }
-});
+import React from 'react'
+
+function ToolBar() {
+  return <div className="texct-dark">Our package</div>
+}
+
+export default ToolBar
